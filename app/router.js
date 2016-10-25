c71234c2aea82e7c6f6b25323349041a55d73e64
Gray Hildreth and Megan Kaluza <gray+megan@>
2016-10-25 16:19:25 -0700

    shows working database

--- app/router.js
+++ app/router.js
@@ -9,6 +9,8 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('gossip');
+  this.route('article', {path: '/article/:article_id'});
+  this.route('article-tile');
 });
 
 export default Router;
---
+++ tests/unit/routes/article-tile-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:article-tile', 'Unit | Route | article tile', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
